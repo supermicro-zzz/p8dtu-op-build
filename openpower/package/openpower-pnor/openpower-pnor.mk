c5b24e0a721d0e2a0bd245744659e7d5534ccc05
Nick Bofferding <bofferdn@us.ibm.com>
2016-10-08 22:17:13 -0500

    SECUREBOOT: Support building PNOR with signed images
    - Update PNOR package config file with option to sign PNOR images
    - Update PNOR package config file with option to enable key transitioning
    - Update PNOR package makefile to support secure signing of images

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -26,8 +26,17 @@ endif
 
 ifeq ($(BR2_OPENPOWER_PNOR_XZ_ENABLED),y)
 OPENPOWER_PNOR_DEPENDENCIES += host-xz
+XZ_ARG=-xz_compression
 endif
 
+ifeq ($(BR2_OPENPOWER_SECUREBOOT_ENABLED),y)
+OPENPOWER_PNOR_DEPENDENCIES += host-sb-signing-utils
+SECUREBOOT_ARG =-secureboot
+endif
+
+ifeq ($(BR2_OPENPOWER_SECUREBOOT_KEY_TRANSITION),y)
+KEY_TRANSITION_ARG=-key_transition
+endif
 
 OPENPOWER_PNOR_INSTALL_IMAGES = YES
 OPENPOWER_PNOR_INSTALL_TARGET = NO
@@ -59,7 +68,9 @@ define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
             -ima_catalog_filename $(BR2_IMA_CATALOG_FILENAME) \
             -openpower_version_filename $(OPENPOWER_PNOR_VERSION_FILE) \
             -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) \
-            $(if ($(BR2_OPENPOWER_PNOR_XZ_ENABLED),y),-xz_compression)
+            -payload_filename $(BR2_SKIBOOT_LID_XZ_NAME) \
+            -pnor_layout $(@D)/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
+            $(XZ_ARG) $(SECUREBOOT_ARG) $(KEY_TRANSITION_ARG)
 
         mkdir -p $(STAGING_DIR)/pnor/
         $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl \
@@ -68,7 +79,7 @@ define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
             -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
             -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
             -outdir $(STAGING_DIR)/pnor/ \
-            -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) \
+            -payload $(OPENPOWER_PNOR_SCRATCH_DIR)/$(BR2_SKIBOOT_LID_XZ_NAME) \
             -bootkernel $(BINARIES_DIR)/$(LINUX_IMAGE_NAME) \
             -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
             -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
